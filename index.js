7a6b71e01517b6547b52feabc3b2047680d23cf9
Arsh Patel <97227357+Arsh510@users.noreply.github.com>
2022-02-14 21:12:51 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,50 @@
+// api = b23a80efd7304a348f6cce8ab13a43b8
+
+// console.log("Welcome to News Website");
+
+// let source = 'the-times-of-india';
+let country = 'in';
+let apiKey = 'b23a80efd7304a348f6cce8ab13a43b8'
+
+let newsAccordion = document.getElementById('newsAccordion');
+const xhr = new XMLHttpRequest();
+// xhr.open('GET', `https://newsapi.org/v2/top-headlines?sources=${source}&apiKey=${apiKey}`, true);
+xhr.open('GET', `https://newsapi.org/v2/top-headlines?country=${country}&apiKey=${apiKey}`, true);
+
+xhr.onload = function () {
+    if (this.status === 200) {
+        let json = JSON.parse(this.responseText);
+        // console.log(json);
+        let articles = json.articles;
+        let newsHtml = "";
+
+        articles.forEach(function(element){
+            // console.log(element[news]);
+            let news = `<div class="accordion" id="newsAccordion">
+                            <div class="accordion-item">
+                                <h2 class="accordion-header" id="heading">
+                                    <div class="accordion-button" type=" " data-bs-toggle="collapse"
+                                        data-bs-target="#collapseOne" aria-expanded=" " aria-controls="collapse">
+                                        ${element["title"]}
+                                    </div>
+                                </h2>
+                                <div id="collapse" class="accordion-collapse collapse show" aria-labelledby="heading"
+                                    data-bs-parent="#accordionExample">
+                                    <div class="accordion-body"> ${element["description"]}. <a href="${element["url"]}" target="_blank">Read more hear</a> </div>
+                                </div>
+                            </div>
+                        </div>`;
+
+            newsHtml += news;
+        });
+        
+        newsAccordion.innerHTML = newsHtml;
+    }
+    else {
+        console.log("Some Error occurs");
+    }
+}
+
+xhr.send();
+
+
